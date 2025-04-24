e0a652a95dc149e2b2a433b0c57faae06259837c
DeltaLoam <akiloamchannel@gmail.com>
2025-04-24 15:10:19 +0700

    final
    fake final

--- Assets/Scripts/Finish Point.cs
+++ Assets/Scripts/Finish Point.cs
@@ -1,37 +1,29 @@
-using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishPoint : MonoBehaviour
 {
-private void OnTriggerEnter2D(Collider2D collision)
-{
-    // Check if the object entering the trigger is the player
-    if (collision.CompareTag("Player"))
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Player reached finish! Loading next scene...");
-        
-        // Call the method to go to next level
-        GoToNextLevel();
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player reached finish! Loading next scene...");
+            GoToNextLevel();
+        }
     }
-}
 
-private void GoToNextLevel()
-{
-    // Get the current scene index
-    int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-    
-    // Check if there is a next scene
-    if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+    private void GoToNextLevel()
     {
-        // Load the next scene
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            Debug.Log("This was the last scene!");
+            // Optionally load credits or return to main menu here
+        }
     }
-    else
-    {
-        // This was the last scene
-        Debug.Log("This was the last scene!");
-        // You could load a credits scene or return to main menu
-    }
-}
 }
